39963753ee683dcb2c51a2be8ce15ae471d431c9
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-12-08 18:46:52 +0100

    Fix creating session

--- src/hooks.ts
+++ src/hooks.ts
@@ -7,14 +7,20 @@ import { SESSION_COOKIE_NAME } from '$lib/constants/sessionCookieName';
 const SESSION_COOKIE_EXPIRY_TIME = 7_140;
 
 export const handle: Handle = async ({ request, resolve }) => {
-	const response = await resolve(request);
 	const cookies = parseCookies(request.headers.cookie ?? '');
 
 	if (cookies[SESSION_COOKIE_NAME]) {
-		return response;
+		return await resolve(request);
 	}
 
 	const sessionId = await createSession();
+	const response = await resolve({
+		...request,
+		headers: {
+			...request.headers,
+			cookie: request.headers.cookies ?? '' + `${SESSION_COOKIE_NAME}=${sessionId};`,
+		},
+	});
 
 	return {
 		...response,
