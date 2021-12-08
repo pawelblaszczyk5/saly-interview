b347a1bdf7c0ef229d8b27500086d21d27692911
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-12-08 17:51:13 +0100

    Layout

---
+++ src/lib/components/icons/Logo.svelte
@@ -0,0 +1,16 @@
+{@html `<svg viewBox="0 0 68 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+	 <path
+		d="M65.1668 18.0833L45.3335 31L65.1668 43.9166V18.0833Z"
+		stroke="black"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M39.6668 12.9167H8.50016C5.37055 12.9167 2.8335 15.2299 2.8335 18.0834V43.9167C2.8335 46.7702 5.37055 49.0834 8.50016 49.0834H39.6668C42.7964 49.0834 45.3335 46.7702 45.3335 43.9167V18.0834C45.3335 15.2299 42.7964 12.9167 39.6668 12.9167Z"
+		stroke="black"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>`}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -20,20 +20,70 @@
 	import 'modern-normalize';
 
 	import '../app.postcss';
+	import Logo from '$lib/components/icons/Logo.svelte';
+	import Add from '$lib/components/icons/Add.svelte';
+	import List from '$lib/components/icons/List.svelte';
+	import { page } from '$app/stores';
 
 	export let anyListCreated: boolean;
 </script>
 
-<header class="layout__header">{anyListCreated}</header>
+<header class="header">
+	<a class="header__logo" href="/" title="Strona główna" sveltekit:prefetch>
+		<Logo />
+	</a>
+	{#if $page.path !== '/lists'}
+		<a class="header__anchor" href="/lists" sveltekit:prefetch>
+			<span class="header__anchor-icon-container">
+				{#if anyListCreated}
+					<List />
+				{:else}
+					<Add />
+				{/if}
+			</span>
+			{anyListCreated ? 'Twoje listy' : 'Utwórz własną listę'}
+		</a>
+	{/if}
+</header>
 
-<main>
+<main class="main">
 	<slot />
 </main>
 
 <style lang="postcss">
-	.layout {
-		&__header {
-			height: 125px;
+	* {
+		--content-side-padding: clamp(1rem, 10vw - 1rem, 7.25rem);
+	}
+	.header {
+		padding: clamp(1.5rem, 2vw + 1rem, 2.5rem) var(--content-side-padding);
+		background-color: #eeeef0;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		&__logo {
+			display: flex;
+			width: clamp(1.625rem, 5vw - 0.5rem, 3.75rem);
 		}
+
+		&__anchor {
+			display: flex;
+			align-items: center;
+			text-decoration: none;
+			gap: 1rem;
+			font-size: clamp(1.25rem, 2vw + 0.75rem, 2rem);
+			color: var(--primary-main-font);
+		}
+
+		&__anchor-icon-container {
+			height: 100%;
+			display: flex;
+			align-items: center;
+			width: clamp(1rem, 2vw + 0.5rem, 2rem);
+		}
+	}
+
+	.main {
+		padding: clamp(1rem, 10vw - 1.5rem, 4.75rem) var(--content-side-padding);
 	}
 </style>
